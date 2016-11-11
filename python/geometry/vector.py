1210c19abf871e2474488a78819a570b7c097063
Marc <marc.venturini@gmail.com>
2016-11-11 23:10:39 +0800

    Merged 2D angle comparison with other vector functions

--- python/geometry/vector.py
+++ python/geometry/vector.py
@@ -41,6 +41,23 @@ def vect2d_dist(p, p1, p2):
     v = vect_sub(p, p1)
     return vect2d_det(line, v) / vect_norm(line) 
 
+# Returns -1 if angle in [0, pi[
+# Returns 1 if angle in [pi, 2pi[
+def vect2d_semicircle(p):
+    if p[1] > 0: return -1
+    if p[1] < 0: return 1
+    if p[0] > 0: return -1
+    if p[0] < 0: return 1
+    assert False
+
+# Angle comparison
+def vect2d_cmp(p1, p2):
+    semicircle = vect2d_semicircle(p1)
+    if semicircle != vect2d_semicircle(p2): return semicircle
+    else: return vect2d_det(p2, p1)
+
+# Basic tests
+
 assert vect_add((1, 0), (0, 1)) == (1, 1)
 assert vect_sub((2, 3), (1, 1)) == (1, 2)
 assert vect_scale((1, 0), 2) == (2, 0)
@@ -54,5 +71,22 @@ assert vect3d_cross((0, 1, 0), (1, 0, 0)) == (0, 0, -1)
 assert vect2d_det((1, 0), (0, 1)) == 1
 assert vect2d_det((0, 1), (1, 0)) == -1
 
+# Test distance to line
+
 assert vect2d_dist((1, 0), (0, -1), (0, 1)) == -1
 assert vect2d_dist((-1, 0), (0, -1), (0, 1)) == 1
+
+# Test angle equality for collinear vectors
+
+assert vect2d_cmp((-1, 2), (-3, 6)) == 0
+
+# Sort list of vectors by angle
+
+import random
+import functools
+
+a = [(1, 0), (2, 2), (0, 3), (-4, 4), (-5, 0), (-6, -6), (0, -7), (8, -8)]
+b = list(a)
+random.shuffle(b)
+b = sorted(b, key=functools.cmp_to_key(vect2d_cmp))
+assert a == b

3e2e5979eede9aff4ef7ce154c3f5e44ff6c1db6
Marc <marc.venturini@gmail.com>
2017-06-09 00:58:06 +0800

    Added Dinitz algorithm for maximum flow

--- python/graph/flow/max_flow.py
+++ python/graph/flow/max_flow.py
@@ -43,6 +43,74 @@ def edmonds_karp(graph, source, sink):
         # Saturate the shortest path
         total_flow += saturate_flow(path)
 
+# Assigns a level to each vertex on a shortest path from source to sink
+# in the residual graph
+# BFS algorithm
+# Proceeds from sink to source to limit the dead-ends during the DFS
+def dinitz_bfs(graph, source, sink):
+    assert source >= 0 and source < graph.vertices_count
+    assert sink >= 0 and sink < graph.vertices_count
+    level = [infinity] * graph.vertices_count
+    level[sink] = 0
+    todo = collections.deque()
+    todo.append(sink)
+    while len(todo) > 0:
+        vertex = todo.popleft()
+        next_level = level[vertex] + 1
+        edges = (edge
+            for edge in graph.edges[vertex]
+            if edge.reverse.capacity > edge.reverse.flow)
+        for edge in edges:
+            next_vertex = edge.head
+            if level[next_vertex] == infinity:
+                level[next_vertex] = next_level
+                if next_vertex == source:
+                    return level
+                todo.append(next_vertex)
+
+# Finds a path from source to sink in a level graph
+# DFS algorithm
+def dinitz_dfs(graph, level, source, sink):
+    assert source >= 0 and source < graph.vertices_count
+    assert sink >= 0 and sink < graph.vertices_count
+    previous_edge = [None] * graph.vertices_count
+    todo = []
+    todo.append(source)
+    while len(todo) > 0:
+        vertex = todo.pop()
+        edges = (edge
+            for edge in graph.edges[vertex]
+            if level[edge.tail] == level[edge.head] + 1
+            if edge.capacity > edge.flow)
+        for edge in edges:
+            next_vertex = edge.head
+            previous_edge[next_vertex] = edge
+            if next_vertex == sink:
+                return find_path(previous_edge, source, sink)
+            todo.append(next_vertex)
+
+# Finds a maximum flow
+# Dinitz algorithm
+# Repeatedly saturates a shortest path in the residual graph
+# Saturates all the shortest paths in a level graph together (blocking flow)
+def dinitz(graph, source, sink):
+    assert source >= 0 and source < graph.vertices_count
+    assert sink >= 0 and sink < graph.vertices_count
+    graph.reset_flow()
+    total_flow = 0
+    while True:
+        # Create the level graph
+        level = dinitz_bfs(graph, source, sink)
+        if level is None:
+            return total_flow
+        while True:
+            # Search for a shortest path
+            path = dinitz_dfs(graph, level, source, sink)
+            if path is None:
+                break
+            # Saturate the shortest path
+            total_flow += saturate_flow(path)
+
 
 if __name__ == "__main__":
 
@@ -61,7 +129,12 @@ if __name__ == "__main__":
     source = 0
     sink = 5
 
+    # Edmonds-Karp
     max_flow = edmonds_karp(g, source, sink)
+    assert max_flow == 19
+
+    # Dinitz
+    max_flow = dinitz(g, source, sink)
     assert max_flow == 19
 
     ### Randomized tests ###
@@ -81,4 +154,7 @@ if __name__ == "__main__":
     source = 0
     sink = vertices_count - 1
 
-    edmonds_karp(g, source, sink)
+    max_flow_edmonds_karp = edmonds_karp(g, source, sink)
+    max_flow_dinitz = dinitz(g, source, sink)
+
+    assert max_flow_edmonds_karp == max_flow_dinitz
